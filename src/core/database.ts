b853a97215d6d3e66792aeae4d49ce066ba8bac1
Antony D'Amico <antonydamico29@gmail.com>
2020-05-10 19:08:36 +0200

    Creating db connection class

---
+++ src/core/database.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+
+export class Database {
+
+    private static options = {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    }
+
+    static connect(uri: string) {
+        mongoose.connect(uri, this.options)
+        mongoose.set('debug', true)
+        mongoose.connection.on('connected', () => {
+            console.log('Mongoose connected');
+        })
+
+        mongoose.connection.on('error', err => {
+            console.log('Mongoose connection error: ' + err);
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
+import { Database } from './core/database';
 import express, { Request, Application, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config()
 const port = parseInt(process.env.SERVER_PORT, 10)
+const mongoUri: string = process.env.MONGO_URI
+
 const app: Application = express()
+Database.connect(mongoUri)
+
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ response: true })
+
+    const Cat = mongoose.model('Cat', { name: String });
+    // const kitty = new Cat({ name: 'Zildjian' });
+    const kitty = new Cat({ name: 'he' });
+    kitty.save().then(() => console.log('meow'));
+
+    // Cat.find((err, cats) => {
+    //     if(err) {
+    //         res.status(200).json({ response: true , err})
+    //         return
+    //     }
+    //     res.status(200).json({ response: true , cats})
+    // })
+    res.status(200).json({ response: true , kitty})
+
 })
 
 app.listen(port, () => {
